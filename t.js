265e361b7d4bec05d840f883489c3ce8a7c50602
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2024-09-27 16:16:58 +0000

    public

--- t.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-var stats = fs.statSync("programs/test.PC");
-var fileSizeInBytes = stats.size;
-console.log(stats);
-
-`
-[SYN][RS][SOH][STX][SOH][NUL][SOH][STX][SOH][ETX][SOH][SOH][SOH][DC3][SOH][NUL][SOH][NUL][RS][ETX][NUL][FF][STX][DC1][GS][ACK][ENQ][ESC][DC2][DC3][GS][BEL][EOT][DC4][STX][HT][ESC][RS][ETX][NUL][DLE][ACK][DC3][STX][BEL][LF][DC4][STX][HT][VT]
-`
