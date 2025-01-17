0cf8105ddb02a62ff7be6fdcfcd5ba36bd8956c7
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2025-01-17 18:21:16 +0000

    spring cleaning

--- compile/compiler.js
+++ compile/compiler.js
@@ -1,9 +1,26 @@
 const fs = require("fs");
 var program = fs.readFileSync(process.argv[2],"utf8");
+
 if(program.match(/-*START\s*\|/gm) == null){
   returnError(0);
 }
-program = program.split(/-START\s*\|\n/g)[1].split("\n")
+
+program = program.split(/-START\s*\|\n/g)[1].split("\n");
+
+var zipped = [];
+
+for(var i =0; i < program.length; i++){
+  if(program[i].split("|")[0].length != program[0].split("|")[0].length){
+    returnError(1);
+  }
+  tex = (((decode(program[i])).toString(2)));
+  tex = ("0".repeat(8-tex.length)+tex).match(/.{1,4}/g);
+  zipped[i] = tex[0]+tex[1];
+}
+
+fs.writeFile(process.argv[3], Buffer.from(btoa(bin2String(zipped)),'base64'), function(err){if(err!=null)returnError(3,err)});
+
+
 function bin2String(array) {
   var result = "";
   for (var i = 0; i < array.length; i++) {
@@ -21,6 +38,7 @@ function returnError(error, extra=""){
     ][error]}.\n\nCode: ${error}`);
   process.exit(1);
 }
+
 function decode(line){
   let number = 0;
   let i = 7;
@@ -35,13 +53,3 @@ function decode(line){
   }
   return number;
 }
-var zipped = [];
-for(var i =0; i < program.length; i++){
-  if(program[i].split("|")[0].length != program[0].split("|")[0].length){
-    returnError(1);
-  }
-  tex = (((decode(program[i])).toString(2)))
-  tex = ("0".repeat(8-tex.length)+tex).match(/.{1,4}/g)
-  zipped[i] = tex[0]+tex[1]
-}
-fs.writeFile(process.argv[3], Buffer.from(btoa(bin2String(zipped)),'base64'), function(err){if(err!=null)returnError(3,err)});
